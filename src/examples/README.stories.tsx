6f33a01add4142a047ed9db5c19a93e3ca1a497f
Daniel Araujo <contact@daniel-araujo.pt>
2026-03-19 23:54:57 +0000

    Add menu card examples

--- src/examples/README.stories.tsx
+++ src/examples/README.stories.tsx
@@ -7,6 +7,9 @@ import { Ui89Card } from "../components/Ui89Card"
 import { Ui89Button } from "../components/Ui89Button"
 import { SceneDecorator } from "../storybook/SceneDecorator"
 import { Ui89TitleBox } from "../components/Ui89TitleBox"
+import { Ui89Popover } from "../components/Ui89Popover"
+import { Ui89MenuCard } from "../components/Ui89MenuCard"
+import { Ui89MenuBar } from "../components/Ui89MenuBar"
 
 const meta: Meta = {
   decorators: [SceneDecorator],
@@ -39,4 +42,90 @@ export const Default: Story = {
       </div>
     </>
   ),
+}
+
+export const WithPopover: Story = {
+  render: (args) => {
+    const [open, setOpen] = React.useState(false)
+
+    const options = [
+      {
+        label: "Option 1",
+        onClick: () => setOpen(false),
+      },
+      { type: "separator" as const },
+      {
+        label: "Close",
+        onClick: () => setOpen(false),
+      },
+    ]
+
+    return (
+      <div style={{ height: "200px" }}>
+        <Ui89Popover
+          open={open}
+          onOpenChange={setOpen}
+          renderContainer={(props) => (
+            <span ref={props.setRef} {...props.props}>
+              <Ui89Button>Open Menu</Ui89Button>
+            </span>
+          )}
+          renderPopover={() => <Ui89MenuCard options={options} />}
+        />
+      </div>
+    )
+  },
+}
+
+export const MenuBarWithPopover: Story = {
+  render: (args) => {
+    function useMenuItem(
+      label: string,
+      options: React.ComponentProps<typeof Ui89MenuCard>["options"],
+    ) {
+      const [open, setOpen] = React.useState(false)
+
+      return {
+        label: (
+          <Ui89Popover
+            open={open}
+            popoverOverflowForce={true}
+            popoverOverflowMaxWidth={200}
+            onOpenChange={setOpen}
+            renderContainer={(props) => (
+              <span ref={props.setRef} {...props.props} onClick={undefined}>
+                {label}
+              </span>
+            )}
+            renderPopover={() => <Ui89MenuCard options={options} />}
+          />
+        ),
+        onClick: () => setOpen(!open),
+      }
+    }
+
+    const items = [
+      useMenuItem("File", [
+        { label: "New", onClick: fn() },
+        { label: "Open", onClick: fn() },
+        { type: "separator" as const },
+        { label: "Exit", onClick: fn() },
+      ]),
+      useMenuItem("Edit", [
+        { label: "Cut", onClick: fn() },
+        { label: "Copy", onClick: fn() },
+        { label: "Paste", onClick: fn() },
+      ]),
+      {
+        label: "Help",
+        onClick: fn(),
+      },
+    ]
+
+    return (
+      <div style={{ height: "200px" }}>
+        <Ui89MenuBar items={items} />
+      </div>
+    )
+  },
 }
